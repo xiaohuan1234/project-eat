f44cdb92de07c614fee9202efaaca909cd072bd0
Haha Coder <xiaohuan.li@gmail.com>
2018-01-03 22:49:42 -0800

    add reset and save buttons to preference menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -100,14 +100,19 @@ $(".js--main-menu").click(function() {
 
 
 $('.js--cuisine').click(function() {		
-	$('.js--cuisine-popup').css("display", "block");
-	$('.js--cuisine-close').css("display", "block");	
-	
+	/*$('.js--cuisine-popup').css("display", "block");
+	$('.js--cuisine-close').css("display", "block");	*/
+	$('.js--cuisine-popup').slideToggle();
+	$('.js--cuisine-close').slideToggle();
+	$('.js--cuisine-reset').slideToggle();
+	$('.js--cuisine-save').slideToggle();
 })
 
 $('.js--cuisine-close').click(function() {		
 	$('.js--cuisine-popup').css("display", "none");	
 	$('.js--cuisine-close').css("display", "none");
+	$('.js--cuisine-reset').css("display", "none");
+	$('.js--cuisine-save').css("display", "none");
 })
 	
 $('.js--calendar').click(function() {		
